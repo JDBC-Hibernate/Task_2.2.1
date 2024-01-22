0a80d380a40055ba8859a6c99f3b3b8928b3be43
Robisho <bigsten@list.ru>
2024-01-22 22:44:30 +0800

    Решение задачи 2.2.1

--- src/main/java/hiber/dao/UserDao.java
+++ src/main/java/hiber/dao/UserDao.java
@@ -5,6 +5,9 @@ import hiber.model.User;
 import java.util.List;
 
 public interface UserDao {
-   void add(User user);
-   List<User> listUsers();
+    <T> void add(T t);
+
+    List<User> listUsers();
+
+    User getUserByCar(String model, int series);
 }
